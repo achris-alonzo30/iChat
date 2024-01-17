47e9566e0d505e4e8825858bb9b7c31c9bd2a3c4
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-01-16 21:59:42 -0400

    1: initial

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const font = Open_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "iChat",
+  description:
+    "Discover new communities and connect with like-minded individuals through extensive network of chat rooms and interest groups.",
+  icons: {
+    icon: "/logo.svg",
+    href: "/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="ichat-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
